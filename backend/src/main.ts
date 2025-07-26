baff1718c2f9bec4eaeaeb634d50ecd79d0db532
kandukuriSandesh <kandukurisandesh1998@gmail.com>
2025-07-26 15:32:25 +0530

    setting global prefix (api) for API routes

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,6 +6,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors(); // allow all origins — for dev only
+  app.setGlobalPrefix('api'); // set global prefix for all routes 
 
   app.useGlobalPipes(new ValidationPipe());
 
