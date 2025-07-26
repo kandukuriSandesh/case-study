35d97c7e84c61836a04315da3990b248c1afc213
kandukuriSandesh <kandukurisandesh1998@gmail.com>
2025-07-26 15:07:29 +0530

    fixing cors error

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(); // allow all origins — for dev only
 
   app.useGlobalPipes(new ValidationPipe());
 
